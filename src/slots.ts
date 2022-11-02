b78c0618d7f127e7ffe247c3dd2d2cace9a6a8ed
Marharyta Perehinets <marharytaperehinets@MacBook-Pro-Marharyta.local>
2022-11-02 14:09:43 +0200

    rewrite slot logic, add highlight

--- src/slots.ts
+++ src/slots.ts
@@ -1,193 +1,173 @@
+import cuid from "cuid";
 import { Application, Container, filters, Graphics, Sprite, Text, TextStyle, Texture } from "pixi.js";
 // 1. add bounce
 // 2. change speed
 interface ReelSymbol {
-    sprite: Sprite;
+    imageIndex: number;
     id: string;
+    sprite: Sprite;
 }
 interface Reel {
-    id: number;
+    id: string;
     container: Container;
-    symbols: ReelSymbol[];
-    // symbolsIds: any[];
+    symbolsById: {
+        [key: ReelSymbolId]: ReelSymbol;
+    };
+    symbolIds: ReelSymbolId[];
+
     position: number;
     previousPosition: number;
     blur: any;
 }
 
 interface Tween {
-    reel: Reel;
-    property: keyof Reel;
-    propertyBeginValue: number;
+    id: string;
+    reelId: Reel["id"];
+    position: number;
     target: number;
-    easing: (time: number) => number;
     time: number;
-    change?: (tween: Tween) => void;
     start: number;
+}
+
+type ReelId = Reel["id"];
+type ReelSymbolId = ReelSymbol["id"];
+type ReelSymbolImageIndex = ReelSymbol["imageIndex"];
+interface Combo {
+    reelId: ReelId;
+    symbolId: ReelSymbolId;
+    imageIndex: ReelSymbolImageIndex;
 }
 
 export class Slots {
     private app: Application;
 
     // layout
-    private history = [];
     private REEL_WIDTH = 170;
     private SYMBOL_SIZE = 150;
     private MINIMUM_TIME = 2500;
-    private finishCount = 0;
-    private slotTextures = [
-        {
-            id: "slot_1",
-            sprite: Texture.from("./assets/slot-1.png"),
-        },
-        {
-            id: "slot_2",
-            sprite: Texture.from("./assets/slot-2.png"),
-        },
-        {
-            id: "slot_3",
-            sprite: Texture.from("./assets/slot-3.png"),
-        },
-        {
-            id: "slot_4",
-            sprite: Texture.from("./assets/slot-4.png"),
-        },
+    private SLOT_TEXTURES = [
+        Texture.from("./assets/slot-1.png"),
+        Texture.from("./assets/slot-2.png"),
+        Texture.from("./assets/slot-3.png"),
+        Texture.from("./assets/slot-4.png"),
     ];
-    private slotTexturesActive = [
-        {
-            id: "slot_1_active",
-            sprite: Texture.from("./assets/slot-1-active.png"),
-        },
-        {
-            id: "slot_2_active",
-            sprite: Texture.from("./assets/slot-2-active.png"),
-        },
-        {
-            id: "slot_3_active",
-            sprite: Texture.from("./assets/slot-3-active.png"),
-        },
-        {
-            id: "slot_4_active",
-            sprite: Texture.from("./assets/slot-4-active.png"),
-        },
+    private ACTIVE_SLOT_TEXTURES = [
+        Texture.from("./assets/slot-1-active.png"),
+        Texture.from("./assets/slot-2-active.png"),
+        Texture.from("./assets/slot-3-active.png"),
+        Texture.from("./assets/slot-4-active.png"),
     ];
 
     // state
     private isRunning = false;
-    private reels: Reel[] = [];
+
+    private reelsById: { [key: ReelId]: Reel } = {};
+    private reelsIds: ReelId[] = [];
+
+    private historyById: { [key: ReelId]: ReelSymbol["id"][] } = {};
+    private history: string[][] = [];
+
     private tweening: Tween[] = [];
+
+    private finishCount = 0;
 
     constructor(app: Application) {
         this.app = app;
         this.render();
     }
 
-    startPlay = () => {
+    private startPlay = () => {
         if (this.isRunning) return;
         this.onTweenStart();
-        for (let i = 0; i < this.reels.length; i++) {
-            const reel = this.reels[i];
+
+        this.reelsIds.forEach((id, index) => {
+            const reel = this.reelsById[id];
+
             const extra = Math.floor(Math.random() * 3);
-            const target = reel.position + 10 + i * 5 + extra;
-            const time = this.MINIMUM_TIME + i * 600 + extra * 600;
-            this.tweenTo({
-                reel,
-                property: "position",
-                target,
-                time,
-                easing: this.backout(0.5),
-                onChange: this.onTweenChange,
-            });
-        }
-    };
-    onTweenStart = () => {
-        console.log("START", this.reels);
-        this.history.forEach((line, i) => {
-            this.history[i] = [];
+
+            const target = reel.position + 10 + index * 5 + extra;
+            const time = this.MINIMUM_TIME + index * 600 + extra * 600;
+
+            this.tweenTo({ target, time, reelId: id, position: reel.position });
         });
+    };
+    private onTweenStart = () => {
+        this.historyById = {};
 
         this.isRunning = true;
         this.finishCount = 0;
     };
 
-    onTweenChange = (tween: Tween) => {
-        // console.log(tween.reel);
-        // tween.reel.id === 2 && console.log(tween.reel);
-        // console.log(tween);
-    };
+    private onTweenChange = (tween: Tween) => {};
 
-    onTweenComplete = () => {
+    private onTweenComplete = () => {
         this.isRunning = false;
 
-        console.log(this.reels);
-
-        this.history.forEach((line, i) => {
-            this.history[i] = line.slice(line.length - 4, line.length - 1);
+        this.reelsIds.forEach((reelId, i) => {
+            const reel = this.historyById[reelId];
+            this.historyById[reelId] = reel.slice(reel.length - 4, reel.length - 1);
         });
 
         this.checkForVerticalCombo();
-        // this.checkForHorizontalCombo();
-        // this.checkForCrossCombo();
-
-        // [horizontal, vertical]
-        // const hoverSymbolList; // [[0,1], [0,2], [0,3], [0, 3], [0, 3]]
+        this.checkForHorizontalCombo();
     };
 
-    checkForCrossCombo = () => {
-        const crossHistory = [];
-    };
+    private checkForVerticalCombo = () => {
+        const dirty: Combo[][] = [];
 
-    checkForVerticalCombo = () => {
-        const combos = [];
-        // const combos = this.getCombos(this.history);
-        for (let y = 0; y < this.history.length; y++) {
-            const line = this.history[y];
-            for (let x = 0; x < line.length; x++) {
-                if (line.every((next) => line[0] === next)) {
-                    combos.push({ i: line[x], x, y });
-                }
+        for (let y = 0; y < this.reelsIds.length; y++) {
+            const reelId = this.reelsIds[y];
+            const reel = this.reelsById[reelId];
+            for (let x = 0; x < this.historyById[reelId].length; x++) {
+                const symbolId = this.historyById[reelId][x];
+                const symbol = reel.symbolsById[symbolId];
+                if (!dirty[y]) dirty[y] = [];
+                dirty[y][x] = { symbolId, reelId, imageIndex: symbol.imageIndex };
             }
         }
 
-        console.log("VERTICAL COMBOS", combos);
-
-        this.hightlightCombos(combos);
+        this.hightlightCombos(this.checkCombos(dirty));
     };
 
-    checkForHorizontalCombo = () => {
-        const combos = [];
+    private checkForHorizontalCombo = () => {
+        const dirty: Combo[][] = [];
 
-        // const reversed = this.history.map((line) => [...line].reverse());
-
-        for (let y = 0; y < this.history.length; y++) {
-            const line = this.history[y];
-            for (let x = 0; x < line.length; x++) {
-                if (this.history.every((next) => line[0] === next[x])) {
-                    combos.push({ i: line[x], y, x: line.length - x - 1 });
-                }
+        for (let y = 0; y < this.reelsIds.length; y++) {
+            const reelId = this.reelsIds[y];
+            const reel = this.reelsById[reelId];
+            for (let x = 0; x < this.historyById[reelId].length; x++) {
+                const symbolId = this.historyById[reelId][x];
+                const symbol = reel.symbolsById[symbolId];
+                if (!dirty[x]) dirty[x] = [];
+                dirty[x][y] = { symbolId, reelId, imageIndex: symbol.imageIndex };
             }
         }
 
-        this.hightlightCombos(combos);
-        console.log("HORIZONTAL COMBOS", combos);
+        this.hightlightCombos(this.checkCombos(dirty));
     };
 
-    hightlightCombos = (combos: Array<{ y: number; x: number; i: number }>) => {
-        combos.forEach(({ x, y, i }) => {
-            this.reels[y].symbols[x + 1].sprite.texture = this.slotTexturesActive[i].sprite;
+    private checkCombos = (dirtyCombos: Combo[][]): Combo[] => {
+        const combos: Combo[] = [];
+
+        dirtyCombos.forEach((line) => {
+            line.forEach((combo) => {
+                if (line.every(({ imageIndex }) => line[0].imageIndex === imageIndex)) {
+                    combos.push(combo);
+                }
+            });
+        });
+
+        return combos;
+    };
+
+    private hightlightCombos = (combos: Combo[]) => {
+        combos.forEach(({ imageIndex, symbolId, reelId }) => {
+            this.reelsById[reelId].symbolsById[symbolId].sprite.texture = this.ACTIVE_SLOT_TEXTURES[imageIndex];
         });
     };
 
-    render = () => {
+    private render = () => {
         const reelContainer = new Container();
-        // const testContainer = new Container();
-        // testContainer.x = 0;
-        // testContainer.y = 0;
-        // const symbol = new Sprite(this.slotTextures[0]);
-        // this.app.stage.addChild(testContainer);
-        // console.log(symbol.x, symbol.y);
-        // testContainer.addChild(symbol);
-        // reelContainer.addChild(symbol);
 
         for (let i = 0; i < 3; i++) {
             const rc = new Container();
@@ -195,10 +175,10 @@ export class Slots {
             reelContainer.addChild(rc);
 
             const reel: Reel = {
-                id: i,
+                id: cuid(),
                 container: rc,
-                symbols: [],
-                // symbolsIds: [],
+                symbolIds: [],
+                symbolsById: {},
                 position: 0,
                 previousPosition: 0,
                 blur: new filters.BlurFilter(),
@@ -208,22 +188,33 @@ export class Slots {
             rc.filters = [reel.blur];
 
             // Build the symbols
-            for (let j = 0; j < 5; j++) {
-                const symbolRandom = Math.floor(Math.random() * this.slotTextures.length);
+            for (let j = 0; j < 4; j++) {
+                const imageIndex = Math.floor(Math.random() * this.SLOT_TEXTURES.length);
 
-                const sprite = new Sprite(this.slotTextures[symbolRandom].sprite);
-                // Scale the symbol to fit symbol area.
+                const sprite = new Sprite(this.SLOT_TEXTURES[imageIndex]);
+
                 sprite.y = j * this.SYMBOL_SIZE;
                 sprite.scale.x = sprite.scale.y = Math.min(
                     this.SYMBOL_SIZE / sprite.width,
                     this.SYMBOL_SIZE / sprite.height,
                 );
-                sprite.x = Math.round((this.SYMBOL_SIZE - sprite.width) / 2);
-                // reel.symbolsIds.push(symbolRandom);
-                reel.symbols.push({ sprite, id: this.slotTextures[symbolRandom].id });
+                // sprite.x = Math.round((this.SYMBOL_SIZE - sprite.width) / 2);
+                sprite.x = 0;
+
+                const symbol = {
+                    imageIndex,
+                    sprite,
+                    id: cuid(),
+                };
+
+                reel.symbolIds.push(symbol.id);
+                reel.symbolsById[symbol.id] = symbol;
+
                 rc.addChild(sprite);
             }
-            this.reels.push(reel);
+
+            this.reelsById[reel.id] = reel;
+            this.reelsIds.push(reel.id);
         }
         this.app.stage.addChild(reelContainer);
 
@@ -295,99 +286,95 @@ export class Slots {
         this.setupTickers();
     };
 
-    setupTickers = () => {
+    private setupTickers = () => {
         this.app.ticker.add((delta: number) => {
             const now = Date.now();
-            const remove = [];
-            for (let i = 0; i < this.tweening.length; i++) {
-                const tween = this.tweening[i];
+            const remove: Tween[] = [];
+
+            this.tweening.forEach((tween) => {
                 const phase = Math.min(1, (now - tween.start) / tween.time);
 
-                tween.reel[tween.property] = this.lerp(tween.propertyBeginValue, tween.target, tween.easing(phase));
+                this.reelsById[tween.reelId].position = this.lerp(tween.position, tween.target, this.backout(phase));
 
-                if (tween.change) tween.change(tween);
+                this.onTweenChange(tween);
+
                 if (phase === 1) {
-                    tween.reel[tween.property] = tween.target;
+                    this.reelsById[tween.reelId].position = tween.target;
                     this.finishCount += 1;
-                    if (this.finishCount === this.reels.length) this.onTweenComplete();
+                    if (this.finishCount === this.reelsIds.length) this.onTweenComplete();
                     remove.push(tween);
                 }
-            }
+            });
+
             for (let i = 0; i < remove.length; i++) {
                 this.tweening.splice(this.tweening.indexOf(remove[i]), 1);
             }
         });
+
         this.app.ticker.add((delta: number) => {
-            // if (!this.isRunning) return;
+            this.reelsIds.forEach((reelId) => {
+                const reel = this.reelsById[reelId];
 
-            for (let i = 0; i < this.reels.length; i++) {
-                const r = this.reels[i];
-                // Update blur filter y amount based on speed.
-                // This would be better if calculated with time in mind also. Now blur depends on frame rate.
-                r.blur.blurY = (r.position - r.previousPosition) * 8;
-                r.previousPosition = r.position;
-                // Update symbol positions on reel.
-                for (let j = 0; j < r.symbols.length; j++) {
-                    const symbol = r.symbols[j];
-                    const prevY = symbol.sprite.y;
-                    symbol.sprite.y = ((r.position + j) % r.symbols.length) * this.SYMBOL_SIZE - this.SYMBOL_SIZE;
-                    if (symbol.sprite.y < 0 && prevY > this.SYMBOL_SIZE) {
-                        // Detect going over and swap a texture.
-                        // This should in proper product be determined from some logical reel.
-                        const nextSymbolIndex = Math.floor(Math.random() * this.slotTextures.length);
-                        const nextSymbol = this.slotTextures[nextSymbolIndex];
-                        // console.log("nextSymbolIndex", nextSymbolIndex);
+                reel.blur.blurY = (reel.position - reel.previousPosition) * 8;
+                reel.previousPosition = reel.position;
 
-                        this.history[i] = [...(this.history[i] || []), nextSymbolIndex];
-                        // if (this.reels[i].id === 2) console.log(nextSymbolIndex);
+                reel.symbolIds.forEach((symbolId, symbolIndex) => {
+                    const prevY = reel.symbolsById[symbolId].sprite.y;
+                    reel.symbolsById[symbolId].sprite.y =
+                        ((reel.position + symbolIndex) % reel.symbolIds.length) * this.SYMBOL_SIZE - this.SYMBOL_SIZE;
 
-                        symbol.sprite.texture = nextSymbol.sprite;
-                        symbol.id = nextSymbol.id;
-                        symbol.sprite.scale.x = symbol.sprite.scale.y = Math.min(
-                            this.SYMBOL_SIZE / symbol.sprite.texture.width,
-                            this.SYMBOL_SIZE / symbol.sprite.texture.height,
-                        );
-                        symbol.sprite.x = Math.round((this.SYMBOL_SIZE - symbol.sprite.width) / 2);
+                    if (reel.symbolsById[symbolId].sprite.y < 0 && prevY > this.SYMBOL_SIZE) {
+                        const imageIndex = Math.floor(Math.random() * this.SLOT_TEXTURES.length);
+                        const nextSymbol = this.SLOT_TEXTURES[imageIndex];
+
+                        this.historyById[reelId] = [...(this.historyById[reelId] || []), symbolId];
+
+                        reel.symbolsById[symbolId].imageIndex = imageIndex;
+                        reel.symbolsById[symbolId].sprite.texture = nextSymbol;
+                        // reel.symbolsById[symbolId].sprite.scale.x = reel.symbolsById[symbolId].sprite.scale.y =
+                        //     Math.min(
+                        //         this.SYMBOL_SIZE / reel.symbolsById[symbolId].sprite.texture.width,
+                        //         this.SYMBOL_SIZE / reel.symbolsById[symbolId].sprite.texture.height,
+                        //     );
+                        // reel.symbolsById[symbolId].sprite.x = 0;
+                        // reel.symbolsById[symbolId].sprite.x = Math.round(
+                        //     (this.SYMBOL_SIZE - reel.symbolsById[symbolId].sprite.width) / 2,
+                        // );
                     }
-                }
-            }
+                });
+            });
         });
     };
 
-    tweenTo = ({
-        reel,
-        property,
+    private tweenTo = ({
         target,
         time,
-        easing,
-        onChange,
+        reelId,
+        position,
     }: {
-        reel: Reel;
-        property: keyof Reel;
         target: number;
         time: number;
-        easing: (time: number) => number;
-        onChange?: (tween: Tween) => void;
+        reelId: ReelId;
+        position: number;
     }) => {
         const tween: Tween = {
-            reel,
-            property,
-            propertyBeginValue: reel[property],
+            id: cuid(),
+            reelId,
             target,
-            easing,
+            position,
             time,
-            change: onChange,
             start: Date.now(),
         };
 
         this.tweening.push(tween);
     };
 
-    lerp = (a1: number, a2: number, time: number) => {
+    private lerp = (a1: number, a2: number, time: number) => {
         return a1 * (1 - time) + a2 * time;
     };
 
-    backout = (amount: number): ((time: number) => number) => {
-        return (time) => --time * time * ((amount + 1) * time + amount) + 1;
+    private backout = (time: number): number => {
+        const AMOUNT = 0.5;
+        return --time * time * ((AMOUNT + 1) * time + AMOUNT) + 1;
     };
 }
